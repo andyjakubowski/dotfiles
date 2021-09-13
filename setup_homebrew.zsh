46d330a4fa412fe0064d90d1cc17593cd14aa06e
Andy Jakubowski <477212+andyjakubowski@users.noreply.github.com>
2021-09-13 15:09:06 +0200

    Add Homebrew setup script
    It will be run via Dotbot’s install executable (as specified in install.conf.yaml).
    - installs Homebrew if it isn’t installed already
    - prompts the user to manually sign into the Mac App Store if not already signed in. The mas-cli formula doesn’t currently support the signin subcommand: https://github.com/mas-cli/mas/issues/164

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -1,3 +1,32 @@
 #!/usr/bin/env zsh
 
-echo "Hello from Homebrew setup!"
+echo "\n<<< Starting Homebrew Setup >>>\n"
+
+if exists brew; then
+  echo "brew exists, skipping install"
+else
+  echo "brew doesn't exist, continuing with install"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
+
+
+
+# TODO: Keep an eye out for a different `--no-quarantine` solution.
+# Currently, you can't do `brew bundle --no-quarantine` as an option.
+# export HOMEBREW_CASK_OPTS="--no-quarantine --no-binaries"
+# https://github.com/Homebrew/homebrew-bundle/issues/474
+
+# HOMEBREW_CASK_OPTS is exported in `zshenv` with
+# `--no-quarantine` and `--no-binaries` options,
+# which makes them available to Homebrew for the
+# first install (before our `zshrc` is sourced).
+
+# Sign in to App Store
+if ! mas account 1> /dev/null; then
+    echo "Please open the App Store app and sign in using your Apple ID..."
+    until mas account 1> /dev/null; do
+        sleep 5
+    done
+fi
+
+brew bundle --verbose
