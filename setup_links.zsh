750bb00deab8f818a591cb7c43964b13690bc840
Andy Jakubowski <477212+andyjakubowski@users.noreply.github.com>
2021-09-15 14:07:49 +0200

    Set up PlistBuddy symbolic link

---
+++ setup_links.zsh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+
+echo "\n<<< Setting up useful links >>>\n"
+
+echo "Putting a link to PlistBuddy on the path"
+ln -fsv /usr/libexec/PlistBuddy /usr/local/bin/plistbuddy
+
+echo "\n<<< ✔ Link setup done >>>\n"
