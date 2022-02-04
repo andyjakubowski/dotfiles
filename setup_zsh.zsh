4e4f28c7a7484683a7eca4f4ee4536ec2c9dd1e3
Andy Jakubowski <477212+andyjakubowski@users.noreply.github.com>
2022-02-04 17:46:22 +0100

    Use $(brew --prefix) for setting the login shell
    Because Homebrew is installed in /opt/homebrew instead of /usr/local on ARM macOS.

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -2,25 +2,31 @@
 
 echo "\n<<< Starting ZSH Setup >>>\n"
 
-# Installation unnecessary; it's in the Brewfile.
+# macOS comes with the Z shell
+# But Homebrew has a newer version, and that’s what we’d like to use
 
+# Installation of Homebrew zsh here unnecessary; it's installed from the Brewfile.
+
+# Add Homebrew zsh to list of login shells in /etc/shells
 # https://stackoverflow.com/a/4749368/1341838
-if grep -Fxq '/usr/local/bin/zsh' '/etc/shells'; then
-  echo '/usr/local/bin/zsh already exists in /etc/shells'
+if grep -Fxq $(brew --prefix)/bin/zsh '/etc/shells'; then
+  echo $(brew --prefix)/bin/zsh already exists in /etc/shells
 else
   echo "Enter superuser (sudo) password to edit /etc/shells"
-  echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+  echo $(brew --prefix)/bin/zsh | sudo tee -a '/etc/shells' >/dev/null
 fi
 
-
-if [ "$SHELL" = '/usr/local/bin/zsh' ]; then
-  echo '$SHELL is already /usr/local/bin/zsh'
+# Set Homebrew zsh as the login shell for this user
+if [ "$SHELL" = $(brew --prefix)/bin/zsh ]; then
+  echo \$SHELL is already $(brew --prefix)/bin/zsh
 else
   echo "Enter user password to change login shell"
-  chsh -s '/usr/local/bin/zsh'
+  chsh -s '$(brew --prefix)/bin/zsh'
 fi
 
-
+# Make built-in zsh (/bin/zsh) the default non-interactive shell
+# Seems like it's not possible to make Homebrew zsh
+# the default here, so we settle for the built-in
 if sh --version | grep -q zsh; then
   echo '/private/var/select/sh already linked to /bin/zsh'
 else
@@ -29,5 +35,5 @@ else
   sudo ln -sfv /bin/zsh /private/var/select/sh
 
   # I'd like for this to work instead.
-  # sudo ln -sfv /usr/local/bin/zsh /private/var/select/sh
+  # sudo ln -sfv $(brew --prefix)/bin/zsh /private/var/select/sh
 fi
