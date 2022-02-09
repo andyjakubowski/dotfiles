c3174dcfac2b99e411a50b44344bf0d510cb8ed6
Andy Jakubowski <477212+andyjakubowski@users.noreply.github.com>
2022-02-09 17:57:33 +0100

    Add sudo to PlistBuddy link command

--- setup_links.zsh
+++ setup_links.zsh
@@ -3,6 +3,6 @@
 echo "\n<<< Setting up useful links >>>\n"
 
 echo "Putting a link to PlistBuddy on the path"
-ln -fsv /usr/libexec/PlistBuddy /usr/local/bin/plistbuddy
+sudo ln -fsv /usr/libexec/PlistBuddy /usr/local/bin/plistbuddy
 
 echo "\n<<< ✔ Link setup done >>>\n"
